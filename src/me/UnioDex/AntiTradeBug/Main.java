95a8175b561bab871d53d462018f216042aa24e2
Yiğit Duman <yigitq@gmail.com>
2018-08-20 16:48:48 +0300

    Found a new bug and completely updated the plugin

--- src/me/UnioDex/AntiTradeBug/Main.java
+++ src/me/UnioDex/AntiTradeBug/Main.java
@@ -3,22 +3,19 @@ package me.UnioDex.AntiTradeBug;
 import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.Zrips.TradeMe.TradeMe;
-import net.md_5.bungee.api.ChatColor;
+import net.ess3.api.events.UserBalanceUpdateEvent;
 
 public final class Main extends JavaPlugin implements Listener {
 
 	private TradeMe tradeMe;
-	private FileConfiguration fc;
 
 	public void onEnable() 
 	{
@@ -29,32 +26,23 @@ public final class Main extends JavaPlugin implements Listener {
 			Bukkit.getPluginManager().disablePlugin(this);
 			return;
 		}
+		Plugin essentials  = Bukkit.getPluginManager().getPlugin("Essentials");
+		if (essentials == null) {
+			Bukkit.getLogger().log(Level.SEVERE, "Essentials couldn't find. UnioAntiTradeBug unloaded.");
+			Bukkit.getPluginManager().disablePlugin(this);
+			return;
+		}
 		tradeMe = TradeMe.getInstance();
 		saveDefaultConfig();
-		fc = this.getConfig();
 	}
 
 	@EventHandler
-	public void onCommandUpdate(PlayerCommandPreprocessEvent event) 
-	{
-		Player p = event.getPlayer();
-
-		if (tradeMe.getUtil().isTrading(p)) {
-			event.setCancelled(true);
-			p.sendMessage(ChatColor.translateAlternateColorCodes('&', fc.getString("messages.commandDenied")));
-			Bukkit.getLogger().log(Level.INFO, p.getName() + " used a command while trading. UnioAntiTradeBug cancelled that.");
-		}
-	}
-
-	@EventHandler
-	public void onChat(AsyncPlayerChatEvent event) 
-	{
-		Player p = event.getPlayer();
-
-		if (tradeMe.getUtil().isTrading(p)) {
-			event.setCancelled(true);
-			p.sendMessage(ChatColor.translateAlternateColorCodes('&', fc.getString("messages.chatDenied")));
-			Bukkit.getLogger().log(Level.INFO, p.getName() + " used a command while trading. UnioAntiTradeBug cancelled that.");
+	public void onMoneyUpdate(UserBalanceUpdateEvent event) {
+		Player player = event.getPlayer();
+		if (tradeMe.getUtil().isTrading(player)) {
+			player.closeInventory();
+			player.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("messages.denied")));
+			Bukkit.getLogger().log(Level.WARNING, player.getName() + "'s money has changed while trading.");
 		}
 	}
 }
